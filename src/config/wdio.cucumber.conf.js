e24128a533fc54edbc2f8128dcb45fbadd76a9f8
Svitlana Savchuk <118730492+Svitlana06@users.noreply.github.com>
2024-09-20 12:39:43 +0300

    Update wdio.cucumber.conf.js

--- src/config/wdio.cucumber.conf.js
+++ src/config/wdio.cucumber.conf.js
@@ -22,7 +22,7 @@ exports.config = {
     
     cucumberOpts: {
         
-        require: ['../../features/support/hook.js',
+        require: ['D:/homework/серпень/homework_2_Webdriver_Know/features/support/hook.js',
             '../../features/step-definitions/require-steps.js'
         ],
         backtrace: false,
