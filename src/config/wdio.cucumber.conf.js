79c76de31d51004b83d06e47936b65d2b2b85989
Svitlana Savchuk <118730492+Svitlana06@users.noreply.github.com>
2024-09-20 13:18:23 +0300

    Update wdio.cucumber.conf.js

--- src/config/wdio.cucumber.conf.js
+++ src/config/wdio.cucumber.conf.js
@@ -23,7 +23,14 @@ exports.config = {
     cucumberOpts: {
         
         require: ['features/support/hook.js',
-            'features/step-definitions/require-steps.js'
+            'features/step-definitions/change_settings_steps.js',
+            'features/step-definitions/create_board_steps.js',
+            'features/step-definitions/create_card_steps.js',
+            'features/step-definitions/create_list_steps.js',
+            'features/step-definitions/edit_profile_steps.js',
+            'features/step-definitions/filter_cards_steps.js',
+            'features/step-definitions/seek_board_steps.js',
+            'features/step-definitions/sign_up_steps.js',
         ],
         backtrace: false,
         requireModule: [],
