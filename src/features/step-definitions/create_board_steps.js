88f965aaabb50deab0f64ce28f45a24517c0b8b1
Svitlana Savchuk <118730492+Svitlana06@users.noreply.github.com>
2024-09-23 10:44:29 +0300

    Update create_board_steps.js

--- src/features/step-definitions/create_board_steps.js
+++ src/features/step-definitions/create_board_steps.js
@@ -1,29 +1,21 @@
 const { Given, When, Then} = require('@cucumber/cucumber');
 const should = require('chai').should();
-const { expect } = require('chai');
 const { pages } = require('../../po');
-const { valuesForFields, url } = require('../../tests/settings');
+const { valuesForFields} = require('../../tests/settings');
 
-Given('I"m on the boards page', async () => {
-    expect((await browser.getUrl()).includes(url.boards));
+
+Given('I am on the workspace settings page', async () => {
+    await pages('basePage').headerComponent.openAccount.click();
+    await pages('basePage').accountWindowComponent.settingsBtn('account').click();
 });
 
-When('I add a new board', async () => {
-    await pages('basePage').headerComponent.addBoardBtn.click();
-    await pages('basePage').boardWindowComponent.settings('type').click();
-    await pages('basePage').boardWindowComponent.settings('name').setValue(valuesForFields.newBoardName);
-    await pages('basePage').boardWindowComponent.submitBtn.isDisplayed();
-    await pages('basePage').boardWindowComponent.submitBtn.click();
+When('I update workspace details', async  () => {
+    await pages('accountPage').settingsComponent.openFrequencyProperty.click();
 
-    await browser.waitUntil(async () => {
-        console.log("I AM HERE")
-        return (await browser.getUrl()).includes(url.newBoardUrl);
-    }, {
-        timeout: 10000,
-    });
-    await pages('boardPage').boardComponent.check('board').isDisplayed();
+    await pages('accountPage').settingsComponent.changeFrequency.waitForExist({ timeout: 10000 })
+    await pages('accountPage').settingsComponent.changeFrequency.click();
 });
 
-Then('a new board should be created and displayed', async () => {
-    (await pages('boardPage').boardComponent.check('board').getText()).should.equal(valuesForFields.newBoardName);
+Then('the workspace settings should be updated successfully', async () => {
+    (await pages('accountPage').settingsComponent.checkFrequency.getText()).should.equal(valuesForFields.checkedFrequently);
 });
